559b10d0645ea3171032182e2a1b15072c292952
dobladamark <122426238+dobladamark@users.noreply.github.com>
2024-11-24 15:22:57 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int counter = 10;
+    while (counter > 3)
+
+    {
+        counter++;
+        cout << "I love you "<<counter<<endl;
+    }
+}
